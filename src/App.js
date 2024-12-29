0242ba40fc2402538ae933923ea70c013e2b9557
AyoubJolti <ayoubkholti939@gmail.com>
2024-12-29 17:07:56 +0100

    add json server make crud with json server

--- src/App.js
+++ src/App.js
@@ -3,35 +3,29 @@ import './App.css';
 import Header from './compenents/Header';
 import Tasks from './compenents/Tasks';
 import AddTask from './compenents/AddTask';
-import { useState } from 'react'
+import { useState ,useEffect} from 'react'
 
 function App() {
   const [showAddTask,setShowAddTask] = useState(false);
-  const [tasks, setTasks] = useState( [
-    {
-      id: 1,
-      text: 'Doctors Appointment',
-      day: 'Feb 5th at 2:30pm',
-      reminder: true,
-    },
-    {
-      id: 2,
-      text: 'Meeting at School',
-      day: 'Feb 6th at 1:30pm',
-      reminder: true,
-    },
-    {
-      id: 3,
-      text: 'Food Shopping',
-      day: 'Feb 5th at 2:30pm',
-      reminder: false,
+  const [tasks, setTasks] = useState( [])
+  const fetchTasks = async () =>{
+    const res = await  fetch('http://localhost:5000/tasks');
+    const data = await res.json();
+    return data;
+  }
+  useEffect(()=>{
+    const getTasks = async () =>{
+     const tasksFromServer = await fetchTasks();
+     setTasks(tasksFromServer);
     }
-  ])
-  const DeleteTask = (id) =>{
+    getTasks();
+  },[])
+  const DeleteTask = async (id) => {
+    await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'DELETE',
+    })
     // eslint-disable-next-line no-restricted-globals
-    if(confirm('Are you sure?')){
-      setTasks(tasks.filter((task)=>task.id !== id));
-    }
+      setTasks(tasks.filter((task) => task.id !== id));
 
   }
   const ToggleReminder = (id) =>{
@@ -41,13 +35,19 @@ function App() {
     )
     )
   }
-  const addTask = (task)=>{
+  const addTask = async (task)=>{
     const lastElement = tasks[tasks.length - 1]
     const id = lastElement.id + 1;
     task.id = id;
-    tasks.push(task)
-    setTasks([...tasks]);
-    alert('Task Added'+task.id);
+    const  res = await fetch(`http://localhost:5000/tasks`, {
+      method: 'POST',
+      headers:{
+        'Content-type':'application/json'
+      },
+      body: JSON.stringify(task),
+    })
+    const  data = await res.json();
+    setTasks([...tasks,data]);
   }
   const onClickAdd =() =>{
     setShowAddTask(!showAddTask);
